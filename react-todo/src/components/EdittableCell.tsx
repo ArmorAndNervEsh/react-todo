c373e374e7e0f72d9577ace0451338c8232661fe
besshokatsuma <nerveshdev@gmail.com>
2024-11-26 09:41:47 +0900

    使ってないものを削除

--- react-todo/src/components/EdittableCell.tsx
+++
@@ -1,6 +0,0 @@
-import { useState } from "react";
-
-export const EditableCell = () => {
-  const [value, setValue] = useState("")
-  return <input value={value} onChange={e => setValue(e.target.value)} />
-}
